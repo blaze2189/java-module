ce9243739f89969e1321ccb705db4258d1adcdfc
blaze2189 <jrgalc2189@gmail.com>
2025-03-02 20:13:43 -0600

    Refactor
    update: refactor processAllCountriesPopulationCities

--- countires-cities-client/src/main/java/com/jlopez/service/ProcessCountriesPopulationCitiesImpl.java
+++ countires-cities-client/src/main/java/com/jlopez/service/ProcessCountriesPopulationCitiesImpl.java
@@ -53,49 +53,41 @@ public final class ProcessCountriesPopulationCitiesImpl implements ProcessCountr
         var yearPopulationList = countriesPopulationCitiesDTO
                 .populationCounts().stream()
                 .map(this::mapPopulationCountDTOToYearPopulation).toList();
+
         return new CityPopulation(countriesPopulationCitiesDTO.city(), yearPopulationList);
     }
 
     /**
-     * Agrupoaciòn de ciudades por paìs
+     * Agrupaciòn de ciudades por paìs
      * */
     private void processCountryPopulationCity(Map<String, CountriesPopulationCities> mapCountriesPopulationCities, CountriesPopulationCitiesDTO countriesPopulationCitiesDTO) {
 
+        //Validaciòn si ya ha sido agregado el paìs al mapa
         var countryExist = mapCountriesPopulationCities
                 .keySet()
                 .stream()
                 .anyMatch(s -> s.equals(countriesPopulationCitiesDTO.country()));
 
-        List<CityPopulation> cityPopulationList = null;
+        //Si el paìs ya existe se toma la informaciòn del paìs en caso contrario se crearà un nuevo registro
+        var cityPopulationList = Optional.ofNullable(mapCountriesPopulationCities.get(countriesPopulationCitiesDTO.country()))
+                .map(CountriesPopulationCities::cityPopulationList)
+                .orElse(new ArrayList<>());
 
-        if (countryExist) {
-            var countryInfo = mapCountriesPopulationCities.get(countriesPopulationCitiesDTO.country());
-            cityPopulationList = Optional.ofNullable(countryInfo.cityPopulationList())
-                    .orElse(new ArrayList<>());
+        //obtenciòn de la poblaciòn por año en cada ciudad
+        var yearPopulationList = countriesPopulationCitiesDTO
+                .populationCounts().stream()
+                .map(this::mapPopulationCountDTOToYearPopulation)
+                .toList();
 
-            var yearPopulationList = countriesPopulationCitiesDTO
-                    .populationCounts().stream()
-                    .map(this::mapPopulationCountDTOToYearPopulation)
-                    .toList();
+        //creaciòn de registro para agregar en la entidad a la estadística de la poblaciòn
+        CityPopulation cityPopulation = new CityPopulation(countriesPopulationCitiesDTO.city(), yearPopulationList);
+        cityPopulationList.add(cityPopulation);
 
-            CityPopulation cityPopulation = new CityPopulation(countriesPopulationCitiesDTO.city(), yearPopulationList);
-            cityPopulationList.add(cityPopulation);
-        } else {
-
-            var yearPopulationList = countriesPopulationCitiesDTO
-                    .populationCounts().stream()
-                    .map(this::mapPopulationCountDTOToYearPopulation)
-                    .toList();
-
-            cityPopulationList = new ArrayList<>();
-
-            CityPopulation cityPopulation = new CityPopulation(countriesPopulationCitiesDTO.city(), yearPopulationList);
-            cityPopulationList.add(cityPopulation);
-
+        //si el país no ha sido agregado al mapa se agega
+        if (!countryExist) {
             CountriesPopulationCities countriesPopulationCities = new CountriesPopulationCities(countriesPopulationCitiesDTO.country(), cityPopulationList);
             mapCountriesPopulationCities.put(countriesPopulationCitiesDTO.country(), countriesPopulationCities);
         }
-
     }
 
     private YearPopulation mapPopulationCountDTOToYearPopulation(PopulationCountDTO populationCountDTO) {
